482d47f5c5127a4fe3bc3f140d3288527878ea7e
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-17 12:24:45 -0400

    feat: ✨ use life cycle method

--- src/ClassState.js
+++ src/ClassState.js
@@ -3,7 +3,20 @@ import React from "react";
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { error: false };
+    this.state = {
+      error: false,
+      loading: false,
+    };
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+    if (this.state.loading) {
+      setTimeout(() => {
+        this.setState({ loading: false });
+        console.log("validando");
+      }, 3000);
+    }
   }
 
   render() {
@@ -12,8 +25,9 @@ class ClassState extends React.Component {
         <h2>Eliminar ClassState</h2>
         <p>Escriba el codigo de seguridad para eliminar ClassState</p>
         {this.state.error && <p>Error: Codigo no valido</p>}
+        {this.state.loading && <p>Cargando...</p>}
         <input type="text" placeholder="Codigo de seguridad" />
-        <button onClick={() => this.setState({ error: !this.state.error })}>
+        <button onClick={() => this.setState({ loading: true })}>
           Confirmar
         </button>
       </React.Fragment>
