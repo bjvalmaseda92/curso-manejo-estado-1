ef336129ee2657e19f3ccb376c3ad7470ed9e946
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-18 14:26:05 -0400

    feat: ⚡️ add validation

--- src/UseState.js
+++ src/UseState.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+const SECURITY_CODE = "paradigma";
 function UseState() {
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
 
@@ -8,21 +10,32 @@ function UseState() {
     console.log("iniciando useEffect");
     if (loading) {
       setTimeout(() => {
+        if (value !== SECURITY_CODE) {
+          setError(true);
+        }
         setLoading(false);
-        console.log("validando");
       }, 3000);
     }
     console.log("finalizando useEffect");
   }, [loading]);
 
+  const onValueChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <React.Fragment>
       <h2>Eliminar UseState</h2>
       <p>Escriba el codigo de seguridad para eliminar UseState</p>
-      {error && <p>Error: Codigo no valido</p>}
+      {error && !loading && <p>Error: Codigo no valido</p>}
       {loading && <p>Cargando...</p>}
 
-      <input type="text" placeholder="Codigo de seguridad" />
+      <input
+        type="text"
+        placeholder="Codigo de seguridad"
+        value={value}
+        onChange={onValueChange}
+      />
       <button onClick={() => setLoading(true)}>Confirmar</button>
     </React.Fragment>
   );
