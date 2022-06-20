5fdc864553870e911a16202b3ca37e7cea22a5a4
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-20 18:29:52 -0400

    refactor: ♻️ change to semi declarative program

--- src/UseState.js
+++ src/UseState.js
@@ -10,32 +10,52 @@ function UseState() {
     deleted: false,
   });
 
+  const onError = () => {
+    setState({
+      ...state,
+      loading: false,
+      error: true,
+    });
+  };
+
+  const onConfirm = () => {
+    setState({
+      ...state,
+      loading: false,
+      error: false,
+      confirmed: true,
+    });
+  };
+
+  const onWrite = (value) => {
+    setState({ ...state, value: value });
+  };
+
+  const onCheck = () => {
+    setState({ ...state, loading: true });
+  };
+
+  const onDelete = () => {
+    setState({ ...state, deleted: true });
+  };
+
+  const onReset = () => {
+    setState({ ...state, confirmed: false, value: "", deleted: false });
+  };
+
   React.useEffect(() => {
     console.log("iniciando useEffect");
     if (state.loading) {
       setTimeout(() => {
         if (state.value !== SECURITY_CODE) {
-          setState({
-            ...state,
-            loading: false,
-            error: true,
-          });
+          onError();
         } else {
-          setState({
-            ...state,
-            loading: false,
-            error: false,
-            confirmed: true,
-          });
+          onConfirm();
         }
       }, 3000);
     }
     console.log("finalizando useEffect");
   }, [state.loading]);
-
-  const onValueChange = (e) => {
-    setState({ ...state, value: e.target.value });
-  };
 
   if (!state.confirmed && !state.deleted) {
     return (
@@ -49,38 +69,24 @@ function UseState() {
           type="text"
           placeholder="Codigo de seguridad"
           value={state.value}
-          onChange={onValueChange}
+          onChange={(e) => onWrite(e.target.value)}
         />
-        <button onClick={() => setState({ ...state, loading: true })}>
-          Confirmar
-        </button>
+        <button onClick={onCheck}>Confirmar</button>
       </React.Fragment>
     );
   } else if (!state.deleted && state.confirmed) {
     return (
       <React.Fragment>
         <p>Esta seguro que desea eliminar?</p>
-        <button onClick={() => setState({ ...state, deleted: true })}>
-          Si, estoy seguro
-        </button>
-        <button
-          onClick={() => setState({ ...state, confirmed: false, value: "" })}
-        >
-          No, me arrepenti
-        </button>
+        <button onClick={onDelete}>Si, estoy seguro</button>
+        <button onClick={onReset}>No, me arrepenti</button>
       </React.Fragment>
     );
   } else {
     return (
       <React.Fragment>
         <p>Elininado con exito</p>
-        <button
-          onClick={() =>
-            setState({ ...state, deleted: false, confirmed: false, value: "" })
-          }
-        >
-          Recuperar
-        </button>
+        <button onClick={onReset}>Recuperar</button>
       </React.Fragment>
     );
   }
